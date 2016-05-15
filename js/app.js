f3d34ac405c809689d7fa08926e3674750b17016
kimberlyl26 <kimberly.lanman@buchanan-edwards.com>
2016-05-15 01:17:36 -0400

    create, update and delete locations with parseObjectType working, more DOM stuff so weather display can be switched depending on location clicked

--- js/app.js
+++ js/app.js
@@ -1,28 +1,45 @@
-
-var api_url = 'http://api.openweathermap.org/data/2.5/';
-
-var params = {
-    prefixes: {
-        weather: 'weather?',
-        zip: 'zip',
-        city: 'q',
-        mode: 'mode',
-        appid: 'appid', 
-        units: 'units'
-    },
-    //Using US for the country, json for the mode, fahrenheit(imperial) for the units 
-    country: 'us' ,
-    mode: 'json',
-    units: 'imperial',
-    api_key:'2b29981d2c60843b266ebfeb3f464e47'
-    
+//Wind chill doesn't have fahrenheit units so this is necessary
+function kelvinToF(k){
+    var f = ((k - 273)* 1.8 + 32.0);
+    return (parseInt(f));
 }
-var $form = $('form');
+var params = {
+    api_url:'http://api.openweathermap.org/data/2.5/weather?',
+    api_key: '2b29981d2c60843b266ebfeb3f464e47'
+
+}
+
+//Using US for the country, json for the mode, fahrenheit(imperial) for the units for now
 
 var WeatherLocation = new ParseObjectType("WeatherLocation");
-// var $savedLocs = $('#saved');
 $(function(){
+    var $form = $('form');
+    //On add location(submit) a location of type zip or city is saved.
+    $form.on('submit', function (e) {
+        e.preventDefault();
+        var $type = $("#loctype").val();
+        var $value = $("#location").val();
+        var $stripped = $value.replace(/ /g, '');
 
+        console.log($stripped);
+        //Location object
+        var locationObj = {type: $type, value: $stripped};
+        WeatherLocation.create(locationObj, function (err, result) {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                locationObj.objectId = result.objectId;
+                console.log(locationObj);
+                renderLocation(locationObj);
+            }
+
+        })
+
+
+    });
+
+    //Gets all locations upon page load and displays them
     WeatherLocation.getAll(function(err, results) {
         if (err) {
             console.log(err);
@@ -31,58 +48,124 @@ $(function(){
             results.forEach(renderLocation);
 
         }
-        $('ul.saved li.location a').click(function (e) {
+
+        //
+        $('.saved li a').on('click', function(e) {
             e.preventDefault();
-            console.log('rqwerqerqe');
-            // getLocationWeather($(this))
-        });
 
-        $form.on('submit', function (e) {
-            e.preventDefault();
-            var $type = $('#loc_type').val();
-            var $value = $('#location').val();
-            var locationObj = {type: $type, value: $value};
-            WeatherLocation.create(locationObj, function (err, result) {
-                if (err) {
-                    console.error(err);
-                }
-                else {
-                    locationObj.objectId = result.objectId;
-                    console.log(locationObj);
-                    renderLocation(locationObj);
+            var $weatherObj = $(this).parent();
+            console.log($weatherObj);
+            var $weatherType = $weatherObj.attr('data-type');
+            console.log($weatherType);
+            var $weatherValue = $weatherObj.attr('data-value');
+            console.log($weatherValue);
+            //There are different url parameters for retrieving weather by zip
+            if($weatherType === 'zip'){
 
-                }
+                getWeatherByZip($weatherValue);
+            }
+            else{
 
-            })
+                getWeatherByCity($weatherValue);
 
+            }
 
         });
+        $('.saved li i').on('click', function(e){
+            e.preventDefault();
+            var $this = $(this);
+            removeLocation($this.parent());
+        })
 
-        function getLocationWeather(locationObj) {
 
-            //onclick use weather api to display popup of current weather for saved location clicked
-        }
 
-        function removeLocation() {
+    function getWeatherByZip(zip){
+        var url = params.api_url +'zip='+zip+',us&mode=json&units=imperial&appid='+params.api_key;
+        console.log(url);
+        getLocationWeather(url);
+
+    }
+
+    function getWeatherByCity(city){
+        console.log(city);
+        var newCity = city.replace(/ /g, '');
+        //Strips spaces from city for url
+        // var newCity = city.replace(/ /g, '');
+        //
+        console.log(newCity)
+        var url = params.api_url +'q='+newCity+',us&mode=json&units=imperial&appid='+params.api_key;
+        console.log(url);
+        getLocationWeather(url);
+    }
+
+    function getLocationWeather(url) {
+        //OPWM defaults to the country in the parameter if zip or city value is a bunch of nonsense.  
+        $.get(url, function (response){
+            console.log(response);
+            var weatherObj = {
+                name: response.name,
+                main: response.weather[0].main + ': '+ response.weather[0].description,
+                temp: 'Temp: '+response.main.temp +' degrees F',
+                wind:
+                {
+                    speed: 'Speed: ' + response.wind.speed,
+                    chill: 'Wind Chill: '+kelvinToF(response.wind.deg)+' degrees F'
+                }
+            };
+
+            var $current =$('div#current');
+            var $weather =$current.find('ul#weather');
+            $current.addClass('hidden');
+            $weather.addClass('hidden');
+
+            //console.log($weather);
+
+            var heading = '<h4>'+weatherObj.name+'</h4>';
+            var weatherHtml = '<li>'+weatherObj.main+'</li><li>'+weatherObj.temp+'</li><li>'+weatherObj.wind.speed+'<br>'+weatherObj.wind.chill+'</li><br><br>';
+
+            $current.html(heading + weatherHtml);
+            $current.removeClass('hidden');
+
+
+     });
+
+    }
+
+        function removeLocation($location) {
             //remove saved location
+            var locationId = $location.data('id');
+
+            WeatherLocation.remove(locationId, function(err) {
+                if (err) {
+                  console.error(err);
+                } else {
+                  $('[data-id="' + locationId + '"]').remove();
+                }
+            });
         }
 
+        
         function updateLocation() {
 
         }
 
-        function renderLocation(locData) {
+
+
+
+    });
+
+    //Uses handle bars to display saved location
+    function renderLocation(locData) {
             var source = $("#loc-template").html();
             var template = Handlebars.compile(source);
             var html = template(locData);
             $('.saved').append(html);
 
-        }
-
-
-    });
+    }
 
 });
+
+
 
 
 
