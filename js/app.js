6384a7192cd05822a63822c6b10774c375554440
kimberlyl26 <kimberly.lanman@buchanan-edwards.com>
2016-06-02 06:10:32 -0400

    small additions

--- js/app.js
+++ js/app.js
@@ -1,13 +1,46 @@
-//Wind chill doesn't have fahrenheit units so this is necessary
-function kelvinToF(k){
-    var f = ((k - 273)* 1.8 + 32.0);
-    return (parseInt(f));
-}
+
+var degToCard = function(deg){
+  if (deg>11.25 && deg<=33.75){
+    return "NNE";
+  }else if (deg>33.75 && deg<=56.25){
+    return "ENE";
+  }else if (deg>56.25 && deg<=78.75){
+    return "E";
+  }else if (deg>78.75 && deg<=101.25){
+    return "ESE";
+  }else if (deg>101.25 && deg<=123.75){
+    return "ESE";
+  }else if (deg>123.75 && deg<=146.25){
+    return "SE";
+  }else if (deg>146.25 && deg<=168.75){
+    return "SSE";
+  }else if (deg>168.75 && deg<=191.25){
+    return "S";
+  }else if (deg>191.25 && deg<=213.75){
+    return "SSW";
+  }else if (deg>213.75 && deg<=236.25){
+    return "SW";
+  }else if (deg>236.25 && deg<=258.75){
+    return "WSW";
+  }else if (deg>258.75 && deg<=281.25){
+    return "W";
+  }else if (deg>281.25 && deg<=303.75){
+    return "WNW";
+  }else if (deg>303.75 && deg<=326.25){
+    return "NW";
+  }else if (deg>326.25 && deg<=348.75){
+    return "NNW";
+  }else{
+    return "N";
+  }
+};
 var params = {
     api_url:'http://api.openweathermap.org/data/2.5/weather?',
     api_key: '2b29981d2c60843b266ebfeb3f464e47'
 
 }
+var icon_url = 'http://openweathermap.org/img/w/';
+
 
 //Using US for the country, json for the mode, fahrenheit(imperial) for the units for now
 
@@ -96,11 +129,12 @@ function getLocationWeather(url) {
             console.log(response);
             var weatherObj = {
                 name: response.name,
-                main: response.weather[0].main + ': ' + response.weather[0].description,
+                main: response.weather[0].main,
+                icon: icon_url + response.weather[0].icon +'.png',
                 temp: 'Temp: ' + response.main.temp + ' degrees F',
                 wind: {
-                    speed: 'Speed: ' + response.wind.speed,
-                    chill: 'Wind Chill: ' + kelvinToF(response.wind.deg) + ' degrees F'
+                    speed: 'Wind Speed: ' + response.wind.speed,
+                    chill: 'Wind Direction: ' + degToCard(response.wind.deg) + ' ('+ parseInt(response.wind.deg)+ ')'
                 }
             };
 
@@ -112,7 +146,7 @@ function getLocationWeather(url) {
             //console.log($weather);
 
             var heading = '<h4>' + weatherObj.name + '</h4>';
-            var weatherHtml = '<li>' + weatherObj.main + '</li><li>' + weatherObj.temp + '</li><li>' + weatherObj.wind.speed + '<br>' + weatherObj.wind.chill + '</li><br><br>';
+            var weatherHtml = '<br><li><h4>'+weatherObj.main+'<img src="'+weatherObj.icon+'" /></h4></li><li>' + weatherObj.temp + '</li><li>' + weatherObj.wind.speed + '<br>' + weatherObj.wind.chill + '</li><br><br>';
 
             $current.html(heading + weatherHtml);
             $current.removeClass('hidden');
